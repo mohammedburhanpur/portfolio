5c55391f2b96275132f17797cacf6543068e2ec6
mohammed <mohburhanpurwala@gmail.com>
2023-12-05 19:46:39 +0530

    about me page commit

--- force-app/main/default/lwc/about/about.js
+++ force-app/main/default/lwc/about/about.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import ABOUTME_IMAGE from "@salesforce/contentAssetUrl/myImage";
 
-export default class About extends LightningElement {}
+export default class About extends LightningElement {
+    aboutMeImageUrl = ABOUTME_IMAGE;
+}
