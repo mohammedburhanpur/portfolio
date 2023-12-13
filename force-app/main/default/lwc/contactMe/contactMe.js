2114e2467616f39220abfc51d8fa98f754109824
mohammed <mohburhanpurwala@gmail.com>
2023-12-13 22:01:40 +0530

    update hero image and contact me image day 4

--- force-app/main/default/lwc/contactMe/contactMe.js
+++ force-app/main/default/lwc/contactMe/contactMe.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import CONTACT_ME_IMAGE from "@salesforce/contentAssetUrl/contactpageimage";
 
-export default class ContactMe extends LightningElement {}
+
+export default class ContactMe extends LightningElement {
+    contactMeImageUrl = CONTACT_ME_IMAGE;
+}
